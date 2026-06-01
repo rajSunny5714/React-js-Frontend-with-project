e3f9680865dfadc1188657ad0a1024581f3f1331
sunnyraj <mrsj5714@gmail.com>
2026-06-01 18:05:22 +0530

    forntend files added

--- ecom-frontend/src/App.jsx
+++ ecom-frontend/src/App.jsx
@@ -1,121 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import React, { useState } from 'react'
 import './App.css'
+import Products from './components/products/Products'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './components/home/Home'
+import Navbar from './components/shared/Navbar'
+import About from './components/About'
+import Contact from './components/Contact'
+import { Toaster } from 'react-hot-toast'
+import Cart from './components/cart/Cart'
+import LogIn from './components/auth/LogIn'
+import PrivateRoute from './components/PrivateRoute'
+import Register from './components/auth/Register'
+import Checkout from './components/checkout/Checkout'
+import PaymentConfirmation from './components/checkout/PaymentConfirmation'
+import AdminLayout from './components/admin/AdminLayout'
+import Dashboard from './components/admin/dashboard/Dashboard'
+import AdminProducts from './components/admin/products/AdminProducts'
+import Sellers from './components/admin/sellers/Sellers'
+import Category from './components/admin/categories/Category'
+import Orders from './components/admin/orders/Orders'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+    <React.Fragment>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={ <Home />}/>
+          <Route path='/products' element={ <Products />}/>
+          <Route path='/about' element={ <About />}/>
+          <Route path='/contact' element={ <Contact />}/>
+          <Route path='/cart' element={ <Cart />}/>
+        
+          <Route path='/' element={<PrivateRoute />}>
+            <Route path='/checkout' element={ <Checkout />}/>
+            <Route path='/order-confirm' element={ <PaymentConfirmation />}/>
+          </Route>
 
-      <div className="ticks"></div>
+          <Route path='/' element={<PrivateRoute publicPage />}>
+            <Route path='/login' element={ <LogIn />}/>
+            <Route path='/register' element={ <Register />}/>
+          </Route>
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
+           <Route path='/' element={<PrivateRoute adminOnly />}>
+            <Route path='/admin' element={ <AdminLayout />}>
+              <Route path='' element={<Dashboard />} />
+              <Route path='products' element={<AdminProducts />} />
+              <Route path='sellers' element={<Sellers />} />
+              <Route path='orders' element={<Orders />} />
+              <Route path='categories' element={<Category />} />
+            </Route>
+          </Route>
+        </Routes>
+      </Router>
+      <Toaster position='bottom-center'/>
+    </React.Fragment>
   )
 }
 
